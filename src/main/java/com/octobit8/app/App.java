ee3a6102a5f25ecfc0fe44f013cb67049045c852
zebafarah <zebafarah85@gmail.com>
2024-02-23 01:20:54 +0530

    V1.0.0

---
+++ src/main/java/com/octobit8/app/App.java
@@ -0,0 +1,13 @@
+package com.octobit8.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
